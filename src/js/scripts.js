1d7de73027f76e96f8fde32f47021f68d73b6b50
fmnguyen <fmnguyen@uw.edu>
2016-05-25 14:42:39 -0700

    Updating project

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,26 @@
 
   'use strict';
 
-  $(function () {
-    // FireShell
+	$(function () {
+
+		$('.filter').click(function() {
+			var filter = $(this).attr('id');
+			console.log(filter);
+			if(filter === 'all') {
+				$('.project-card').fadeIn('fast');
+			} else {
+				$('.project-card').each(function (){
+					if(!$(this).hasClass('.' + filter)) {
+						$(this).fadeOut('fast');
+					}
+				});
+				$('.'+ filter).fadeIn();
+			}
+			$('.filters-list li').removeClass('active');
+			$(this).addClass('active');
+			return false;
+		});
+
   });
 
 })(jQuery, window, document);
